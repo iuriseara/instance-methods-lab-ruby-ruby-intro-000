7a6d3412ea9a1e263b5c67cce30023bc1fd8509d
fearless-command-2358 <iuriseara@gmail.com>
2020-05-08 01:31:31 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,8 @@
+class Dog
+  def bark
+  puts "Woof!"
+  end
+end
+
+sit = Dog.new
+puts "The Dog is sitting"
---
+++ lib/person.rb
@@ -0,0 +1,8 @@
+class Person
+  def talk
+    puts "Hello World!"
+  end
+end
+
+walk = Person.new
+puts "The Person is walking"
